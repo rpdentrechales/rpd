7981f2583ae2747cb1e4714f58e160840c1f45c4
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-11 14:11:40 +0000

    Updating files

--- views/teste_vendedoras.py
+++ views/teste_vendedoras.py
@@ -17,6 +17,8 @@ def load_df(worksheet):
 
 url_parameters = st.query_params
 
+st.markdown("# RPD - Área das vendedoras")
+
 if "id" in url_parameters:
 
   id_vendedora = st.query_params["id"]
@@ -36,16 +38,10 @@ if carregar_pag_vendedora:
   nome_vendedora = dados_vendedora["nome"].values[0]
   email_vendedora = dados_vendedora["email"].values[0]
   loja_vendedora = dados_vendedora["loja"].values[0]
-  id_vendedora = dados_vendedora["id_vendedora"].values[0]
 
-  header_1, header_2 = st.columns(2)
+  st.header(f"Olá, {nome_vendedora}")
+  st.write(f"Sua loja é a {loja_vendedora}")
+  st.write(f"Seu email é {email_vendedora}")
 
-  with header_1:
-
-    st.markdown("# RPD - Área das vendedoras")
-
-  with header_2:
-    st.header(f"Olá, {nome_vendedora}")
-  
 else:
-  st.markdown("# Essa página não existe")
+  st.write("Essa página não existe")
